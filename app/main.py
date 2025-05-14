d347357eb6eeda252c178c3bdf54acf3f15714c8
r0maf <r9888582703@gmail.com>
2025-05-14 21:10:19 +0300

    some preparations

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+html = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def main():
-    
-    html = Jinja2Templates("templates")
-    return html.TemplateResponse(name="index.html")
+def base_page(request: Request):
+    return html.TemplateResponse(name="index.html", request=request)
