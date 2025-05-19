c855d7cd37a8d7daf348d9d8fbda471c39e387ff
r0maf <r9888582703@gmail.com>
2025-05-19 14:32:11 +0300

    added creation of db tables

--- app/database/db.py
+++ app/database/db.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
+from .models import Base
 
 engine = create_engine("postgresql+psycopg2://postgres:roma123@192.168.0.115:5432/my-server-db")
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
+    Base.metadata.create_all(engine)
     db = SessionLocal()
     try:
         yield db
