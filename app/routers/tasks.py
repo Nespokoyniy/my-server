de097a0378f338b5d13722624ce294deb89bfca8
r0maf <r9888582703@gmail.com>
2025-05-22 20:50:17 +0300

    added delete func and fixed several errors

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -1,42 +1,54 @@
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from fastapi.routing import APIRouter
 from ..database.database import get_db, Session
 from ..validation import schemas
 from ..database import models
-from sqlalchemy import select, update
+from sqlalchemy import delete, select, update
 
 router = APIRouter(prefix="/api/tasks")
 
 
-@router.post("/")
+@router.post("/", status_code=200)
 def new_task(body: schemas.Task, db: Session = Depends(get_db)):
     task = models.Task(**body.model_dump())
     db.add(task)
     db.commit()
 
 
-@router.get("/")
+@router.get("/", status_code=200)
 def get_tasks(db: Session = Depends(get_db)):
     tasks = db.execute(
         select(models.Task.name, models.Task.description, models.Task.priority,)
-    ).all()
+    ).mappings().all()
     return tasks
 
 
-@router.get("/{id}")
+@router.get("/{id}", status_code=200)
 def get_task(id: int, db: Session = Depends(get_db)):
     task = db.execute(
         select(models.Task.name, models.Task.description, models.Task.priority).where(models.Task.id == id)
-    ).all()
+    ).mappings().first()
+    
+    if not task:
+        raise HTTPException
     
     return task
 
 
-@router.put("/{id}")
+@router.put("/{id}", status_code=200)
 def update_task(body: schemas.Task, id: int, db: Session = Depends(get_db)):
-    task = db.execute(
+    db.execute(
         update(models.Task).where(models.Task.id == id).values(**body.model_dump())
     )
     db.commit()
      
-    return task
+    return
+
+@router.delete("/{id}", status_code=204)
+def delete_task(id: int, db: Session = Depends(get_db)):
+    db.execute(
+        delete(models.Task).where(models.Task.id == id)
+    )
+    db.commit()
+    
+    return 
