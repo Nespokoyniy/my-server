085be4bcdf135393434c75424a2bd592adcedbf1
r0maf <r9888582703@gmail.com>
2025-07-02 20:52:29 +0300

    changed conftest, added new function to test_tasks.py. Project doesn't work because of an unexpected error. It will be fixed as soon as possible

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,32 +7,39 @@ from sqlalchemy.orm import sessionmaker
 from backend.app.config import settings as ss
 from backend.app.main import app
 
+
 @pytest.fixture
-def client():
+def test_db():
     engine = create_engine(ss.DB_URL)
     SessionLocal = sessionmaker(autoflush=False, bind=engine)
     test_db = SessionLocal()
+    return test_db
+
+
+@pytest.fixture
+def client(test_db):
     app.dependency_overrides[get_db] = lambda: test_db
     with TestClient(app) as test_client:
         yield test_client
     app.dependency_overrides.clear()
 
+
 @pytest.fixture
 def register(client: TestClient):
     client.post(
-        "http://localhost:8000/api/auth/register",
+        "/api/auth/register",
         json={
             "name": "example",
-            "email": "example@gmail.com",
             "password": "example123",
+            "email": "example@gmail.com",
         },
     )
 
+
 @pytest.fixture
 def token(client: TestClient, register):
-    
     token = client.post(
-        "http://localhost:8000/api/auth/login",
+        "/api/auth/login",
         data={"username": "example", "password": "example123"},
     )
     print(token)
--- tests/test_routers/test_tasks.py
+++ tests/test_routers/test_tasks.py
@@ -10,3 +10,11 @@ class TestGetTasks():
     def test_get_tasks_auth_returns_401(self, client: TestClient):
         resp = client.get("/api/tasks")
         assert resp.status_code == 401
+        
+class CompleteUncompleteTask():
+    
+    def test_complete_uncomplete_task_returns_200(self, token, client: TestClient):
+        client.post("/api/tasks", json={"name": "task", "description": "new task", "priority": 6})
+        task = client.put("/api/tasks/1/")
+        assert task.status_code == 200
+        assert task.is_completed == True
